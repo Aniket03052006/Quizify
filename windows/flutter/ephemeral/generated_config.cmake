a787112cdcee287a16c08f2ec916d0f604d3a66a
Aniket03052006 <aniket.mandal@iitg.ac.in>
2025-07-15 23:35:25 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\DELL\\flutter_windows_3.29.2-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Desktop\\Aniket - IIT G\\AppDev\\alcher2" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\DELL\\flutter_windows_3.29.2-stable\\flutter"
+  "PROJECT_DIR=D:\\Desktop\\Aniket - IIT G\\AppDev\\alcher2"
+  "FLUTTER_ROOT=C:\\DELL\\flutter_windows_3.29.2-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Desktop\\Aniket - IIT G\\AppDev\\alcher2\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Desktop\\Aniket - IIT G\\AppDev\\alcher2"
+  "FLUTTER_TARGET=D:\\Desktop\\Aniket - IIT G\\AppDev\\alcher2\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Desktop\\Aniket - IIT G\\AppDev\\alcher2\\.dart_tool\\package_config.json"
+)
